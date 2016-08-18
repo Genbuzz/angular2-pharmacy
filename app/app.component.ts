fcc6a5cbba5d79f5d2be4b2d53d73ef6ccbb016a
Ivan Bravo <enigmavoodoo@gmail.com>
2016-08-18 22:15:30 +0200

    Added medicines component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { MedicinesComponent } from './medicines/medicines.component';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>Bienvenido al mundo Angular 2</h1>'
+    template: `
+    <h1>{{name}}</h1>
+    <my-medicines></my-medicines>
+    `,
+    directives: [MedicinesComponent]
 })
-
-export class AppComponent { }
+export class AppComponent { 
+    // We need to create 'name' property
+    name = "Nombre de la medicina";
+}
---
+++ app/medicines/medicines.component.ts
@@ -0,0 +1,32 @@
+/**
+ * We are gonna create a list of medicines, so
+ * we need to create a component first.
+ * 
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+import { Medicine } from './medicine.model';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-medicines',
+    templateUrl: 'medicines.component.html',
+    styleUrls: ['medicines.component.css']
+})
+export class MedicinesComponent implements OnInit {
+    medicines: Medicine[];     
+    
+    constructor() { }
+
+    ngOnInit() {
+        // List of medicines
+        this.medicines = [
+            { 'id': 11, 'name': 'Aspirina' },
+            { 'id': 12, 'name': 'Ibuprofeno' },
+            { 'id': 13, 'name': 'Diazepan' },
+            { 'id': 14, 'name': 'Clamoxil' },
+            { 'id': 15, 'name': 'Lexatin' }
+        ];
+     }
+}
