05f34b09b5fc97269bbedab9f02a57be61654830
Ivan Bravo <enigmavoodoo@gmail.com>
2016-08-18 19:58:31 +0200

    Deleted wrong files

--- app/Medicine.ts
+++
@@ -1,8 +0,0 @@
-export interface Medicine{
-    getCode(): number;
-    getName(): string;
-    getPrice(): number;
-    getPrescription(): boolean;
-    setPrice(nPrice: number): void;
-    setPrescription(nPrescription: boolean): void;
-}
